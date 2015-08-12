0b40fa1afc30188ee89966dc05afb2ae16ebaac9
Koushik Dutta <koushd@gmail.com>
2015-08-11 20:01:05 -0700

    port

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('vysor.io');
-});
+var port = process.env.PORT || 3000;
 
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
