26eb24f22460fdcfe8e7ab387a0d90d94eb8c250
Koushik Dutta <koushd@gmail.com>
2015-08-14 12:27:19 -0700

    intiial template

--- app.js
+++ app.js
@@ -1,17 +1,67 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-var port = process.env.PORT || 3000;
 
+app.get('/troubleshooting/ios', function(req, res) {
+  res.redirect('https://github.com/koush/support-wiki/wiki/AllCast-Wiki');
+})
 
-app.get('/', function (req, res) {
-  res.send('<html><head><meta name="google-site-verification" content="AbntZigDG-zAMsB3C8oojbXtSl9XJQvA800wIrarN6g" /></head><body>vysor.io</body></html>');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
-var server = app.listen(port, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+module.exports = app;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+var debug = require('debug')('allcast-site');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
 });
---
+++ public/README.txt
@@ -0,0 +1,33 @@
+Fractal by HTML5 UP
+html5up.net | @n33co
+Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+
+A simple landing page template for showcasing mobile apps (although it'll definitely work
+for other stuff if you get rid of the phone :) Lightweight, fully responsive, and built on
+Skel 3, Sass, and flexbox.
+
+Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
+you can use for pretty much whatever.
+
+(* = not included)
+
+AJ
+n33.co @n33co dribbble.com/n33
+
+
+Credits:
+
+	Demo Images:
+		Unsplash (unsplash.com)
+
+	Icons:
+		Font Awesome (fortawesome.github.com/Font-Awesome)
+
+	Other:
+		jQuery (jquery.com)
+		html5shiv.js (@afarkas @jdalton @jon_neal @rem)
+		CSS3 Pie (css3pie.com)
+		Respond.js (j.mp/respondjs)
+		jquery.scrolly (n33.co)
+		Skel (skel.io)
