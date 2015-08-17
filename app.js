4877eab6e835027c6be248c65ae6545bc9e59ba8
Koushik Dutta <koushd@gmail.com>
2015-08-16 23:44:44 -0700

    stuff works i guess

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-
-app.get('/troubleshooting/ios', function(req, res) {
-  res.redirect('https://github.com/koush/support-wiki/wiki/AllCast-Wiki');
-})
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,7 +17,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/redirect/:registrationId/:secret', express.static(path.join(__dirname, 'public')));
+app.use('/view/:registrationId/:secret', express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res) {
+  res.send('vysor.io');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,7 +55,6 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
 
