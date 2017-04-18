a13f84f5449a6c41605f750b97430e6ce40a370b
Koushik Dutta <koushd@gmail.com>
2017-04-18 12:12:25 -0700

    fix

--- app.js
+++ app.js
@@ -72,11 +72,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-var debug = require('debug')('vysor-site');
-
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
-});
