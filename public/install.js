28d19eeadfd28851dc18f318d11413be627a834e
Koushik Dutta <koushd@gmail.com>
2017-04-22 15:12:33 -0700

    download links

---
+++ public/install.js
@@ -0,0 +1,13 @@
+function fallbackInstall() {
+  window.location = 'https://chrome.google.com/webstore/detail/vysor-beta/gidgenkbbabolejbgbpnhbimgjbffefm?authuser=1'
+}
+function tryInlineInstall() {
+  if (!window.chrome || !window.chrome.webstore || !window.chrome.webstore.install)
+    return fallbackInstall();
+
+  chrome.webstore.install('https://chrome.google.com/webstore/detail/gidgenkbbabolejbgbpnhbimgjbffefm',
+  fallbackInstall, fallbackInstall)
+}
+function openDownload() {
+  window.location = '/download.html?return=' + encodeURIComponent(window.location.toString())
+}
