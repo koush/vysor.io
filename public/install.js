1bb5fe20283365901245bb8aa30fd09e1d060541
Koushik Dutta <koushd@gmail.com>
2020-04-23 18:24:32 -0700

    fix

--- public/install.js
+++ public/install.js
@@ -1,13 +1,3 @@
-function fallbackInstall() {
-  window.location = 'https://chrome.google.com/webstore/detail/vysor-beta/gidgenkbbabolejbgbpnhbimgjbffefm?authuser=1'
-}
-function tryInlineInstall() {
-  if (!window.chrome || !window.chrome.webstore || !window.chrome.webstore.install)
-    return fallbackInstall();
-
-  chrome.webstore.install('https://chrome.google.com/webstore/detail/gidgenkbbabolejbgbpnhbimgjbffefm',
-  fallbackInstall, fallbackInstall)
-}
 function openDownload(ret) {
   window.location = '/download?return=' + (ret ? ret : encodeURIComponent(window.location.toString()));
 }
